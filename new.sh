fe26f887b768bc6717793582f61d8a8063deae90
SeongJae Park <sj38.park@gmail.com>
2020-08-23 11:11:39 +0200

    new: Check return code directly using 'if'
    This change made to fix the shellcheck warnings.
    
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- new.sh
+++ new.sh
@@ -15,8 +15,7 @@ then
 	exit
 fi
 
-git pull origin master
-if [ $? -ne 0 ]
+if ! git pull origin master
 then
 	echo "Failed to pull remote"
 	exit 1
@@ -27,8 +26,7 @@ echo $ts > ./.ts
 git add ./.ts
 git commit -m "$1" > /dev/null
 
-git push origin master
-if [ $? -ne 0 ]
+if ! git push origin master
 then
 	echo "Failed to push to remote.  Do push manually."
 	exit 1
