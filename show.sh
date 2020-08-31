41e0f472f54a8987bb4c6803f41144646c39b465
SeongJae Park <sj38.park@gmail.com>
2020-08-31 10:05:44 +0200

    show: Implement random thought picking
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- show.sh
+++ show.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-git log --pretty="%ad%n%n%B%n%n"
+nr_thoughts=$(git rev-list --count HEAD)
+to_skip=0
+
+if [ $# -eq 1 ] && [ "$1" = "--random" ]
+then
+	to_skip=$((RANDOM % nr_thoughts))
+	nr_thoughts=1
+fi
+git log --pretty="%ad%n%n%B%n%n" --skip=$to_skip --max-count=$nr_thoughts
