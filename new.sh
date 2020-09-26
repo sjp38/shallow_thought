3ebec114b61793a8639c885fe0572d2791152f10
SeongJae Park <sj38.park@gmail.com>
2020-09-26 11:07:15 +0200

    new: Implement hashtag feature
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- new.sh
+++ new.sh
@@ -6,9 +6,15 @@ then
 	exit 1
 fi
 
+msg=$1
+
 echo "This will log your thought:"
-echo "$1"
+echo "$msg"
 echo
+
+tags=$(echo "$msg" | grep -o -E "#[a-zA-Z0-9_-]+" | tr '\n' ' ')
+echo "found tags: $tags"
+
 read -rp "Okay? [Y/n] " answer
 if [ "$answer" = "n" ]
 then
@@ -21,9 +27,27 @@ then
 	exit 1
 fi
 
+tags+=" all"
+
+if [ ! -d tags ]
+then
+	mkdir tags
+fi
+
+for tag in $tags
+do
+	tagfile="tags/$tag"
+	if [ ! -e $tagfile ]
+	then
+		touch $tagfile
+	fi
+	nr_thoughts=$(( $(cat $tagfile) + 1))
+	echo $nr_thoughts > $tagfile
+done
+
 ts=$(( $(cat ./.ts) + 1))
 echo $ts > ./.ts
-git add ./.ts
+git add ./.ts ./tags
 git commit -m "$1" > /dev/null
 
 if ! git push origin master
