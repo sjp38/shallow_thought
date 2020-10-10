831132d7134fc888de856b28c1a324776c43ee0a
SeongJae Park <sj38.park@gmail.com>
2020-10-10 09:14:29 +0200

    show: Implement '-h', '--help' options
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- show.sh
+++ show.sh
@@ -1,9 +1,40 @@
 #!/bin/bash
 
+function pr_usage {
+	echo "Usage: $0 [OPTION]..."
+	echo
+	echo "OPTION"
+	echo "  --random	Show random thought"
+	echo "  -h, --help	Show this usage"
+}
+
+random=false
+while true; do
+	case "$1" in
+	"--random")
+		random=true
+		shift 1
+		continue
+		;;
+	"--help")
+		pr_usage
+		exit 0
+		;;
+	"-h")
+		pr_usage
+		exit 0
+		;;
+	*)
+		break
+		;;
+	esac
+done
+
+
 nr_thoughts=$(git rev-list --count HEAD)
 to_skip=0
 
-if [ $# -eq 1 ] && [ "$1" = "--random" ]
+if "$random"
 then
 	to_skip=$((RANDOM % nr_thoughts))
 	nr_thoughts=1
