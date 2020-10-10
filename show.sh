9b58b3fcc60acecff7b302aad89c014df2e466ab
SeongJae Park <sj38.park@gmail.com>
2020-10-10 09:25:20 +0200

    show: Implement tag specific thoughts show
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- show.sh
+++ show.sh
@@ -5,6 +5,7 @@ function pr_usage {
 	echo
 	echo "OPTION"
 	echo "  --random	Show random thought"
+	echo "  --tags <tags>	Show thoughts of <tags>"
 	echo "  -h, --help	Show this usage"
 }
 
@@ -14,6 +15,11 @@ while true; do
 	"--random")
 		random=true
 		shift 1
+		continue
+		;;
+	"--tags")
+		tags=$2
+		shift 2
 		continue
 		;;
 	"--help")
@@ -30,6 +36,16 @@ while true; do
 	esac
 done
 
+tags_option=""
+for tag in $tags
+do
+	tags_option+=" tags/$tag"
+done
+
+if [ ! -z "$tags_option" ]
+then
+	tags_option=" -- $tags_option"
+fi
 
 nr_thoughts=$(git rev-list --count HEAD)
 to_skip=0
@@ -39,4 +55,5 @@ then
 	to_skip=$((RANDOM % nr_thoughts))
 	nr_thoughts=1
 fi
-git log --pretty="%ad%n%n%B%n%n" --skip=$to_skip --max-count=$nr_thoughts
+git log --pretty="%ad%n%n%B%n%n" --skip=$to_skip --max-count=$nr_thoughts \
+	$tags_option
