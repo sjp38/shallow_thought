5bdfb3e43192e99763f8623c0350d0b581e2d18a
SeongJae Park <sj38.park@gmail.com>
2020-10-17 11:55:06 +0200

    restore_backup: Check validity of given backup name
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- restore_backup.sh
+++ restore_backup.sh
@@ -8,6 +8,12 @@ fi
 
 backup=$1
 
+if ! git branch -r | grep --quiet "^  origin/$backup$"
+then
+	echo "Invalid backup"
+	exit 1
+fi
+
 backup_current=$(date +%Y-%m-%d-%H-%M-%S)
 echo "Backup current thoughts in name of $backup_current"
 git branch "$backup_current"
