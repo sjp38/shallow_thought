e435b38a2238f07d8cf37d36f3b304966192eb9e
SeongJae Park <sj38.park@gmail.com>
2020-10-17 08:24:33 +0200

    Implement backup restore feature
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

---
+++ restore_backup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]
+then
+	echo "Usage: $0 <backup>"
+	exit 1
+fi
+
+backup=$1
+
+backup_current=$(date +%Y-%m-%d-%H-%M-%S)
+echo "Backup current thoughts in name of $backup_current"
+git branch "$backup_current"
+git push origin "$backup_current"
+
+if ! git reset --hard "origin/$backup"
+then
+	echo "Failed restoring $backup"
+	exit 1
+fi
+
+echo
+echo "Push restored thoughts"
+git push origin master --force
