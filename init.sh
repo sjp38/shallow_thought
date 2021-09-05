392336f6144e69060c56567c0779367c34a807cd
SeongJae Park <sj38.park@gmail.com>
2021-09-05 12:05:21 +0200

    init: Do nothing if usage is wrong
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- init.sh
+++ init.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-git init
-echo 0 > ./.ts
-
 if [ $# -lt 1 ]
 then
 	echo "Usage: $0 <remote storage url>"
 	exit 1
 fi
+
+git init
+echo 0 > ./.ts
 
 REMOTE_URL="$1"
 git remote add origin "$REMOTE_URL"
